436870b221f8cbb7b43ded13d7859af8fd78fd14
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2021-12-01 18:36:19 +0200

    Update LAB-6.2.cpp

--- LAB-6.2/LAB-6.2.cpp
+++ LAB-6.2/LAB-6.2.cpp
@@ -3,28 +3,28 @@
 // Сушинський Ігор
 // Лабораторна робота № 6.2
 // Опрацювання одновимірних масивів ітераційними та рекурсивними способами.
-// Варіант 22
+// Варіант 28
 // Ітераційний спосіб
 
 #include <iostream>
 
 using namespace std;
 
-int Count(int* a, int n) // функція обчислення к-ті елементів
+int Count(int* a, int n)
 {
 	int count = 0;
 	for (int i = 0; i < n; i++)
-		if (a[i] % 2 == 0)
+		if ((a[i] % 2) != 0)
 			count++;
 	return count;
 }
 
 int main()
 {
-	const int n = 33; // поточна розмірність масиву n = 1..33
+	const int n = 5; // поточна розмірність масиву n = 1..32
 	int a[n]; // заданий масив
 
-	cout << "count = " << Count(a, n) << "\n";
+	cout << "count = " << Count(a, n) << endl;
 
 	return 0;
 }
