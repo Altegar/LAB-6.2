81e99755bafcd61c986537a10137aab4bcce8c2d
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2021-12-16 15:59:59 +0200

    Update LAB-6.2.cpp

--- LAB-6.2/LAB-6.2.cpp
+++ LAB-6.2/LAB-6.2.cpp
@@ -7,22 +7,43 @@
 // Ітераційний спосіб
 
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
 
-int Count(int* a, int n)
+void Random(int* a, const int n, const int x, const int y)
+{
+	for (int i = 0; i < n; i++) // i - додаткова змінна
+		a[i] = rand() % (y - x + 1) + x;
+}
+
+int Count(int* a, const int n)
 {
 	int count = 0;
 	for (int i = 0; i < n; i++)
-		if ((a[i] % 2) != 0)
+		if (a[i] % 2 != 0)
 			count++;
 	return count;
 }
 
+void Print(int* a, const int n) // форматне виведення
+{
+	for (int i = 0; i < n; i++)
+		cout << setw(3) << a[i] << " ";
+	cout << "\n";
+}
+
 int main()
 {
-	const int n = 5; // поточна розмірність масиву n = 1..32
+	srand((unsigned)time(0)); // ініціалізація генератора випадкових чисел
+
+	const int n = 12; // поточна розмірність масиву n = 1..12
 	int a[n]; // заданий масив
+	int x = 2, y = 47;
+
+	Random(a, n, x, y);
+	cout << "Random = "; Print(a, n);
 
 	cout << "count = " << Count(a, n) << endl;
 
